116c88986719d31b24759d8238cc9a7a1da9efe0
Luc Garrabos <92851982+LGARRABOS@users.noreply.github.com>
2023-10-30 11:31:37 +0100

    yé suis un commit

--- src/serveur.ts
+++ src/serveur.ts
@@ -1,9 +1,11 @@
 import * as http from 'http';
+import * as os from 'os';
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/ping') {
     // Récupérez les en-têtes de la requête
     const headers = req.headers;
+    console.log(os.hostname());
 
     // Envoyez les en-têtes en tant que JSON directement
     res.writeHead(200, {
